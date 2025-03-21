26362cb8a40c4ac1b76a0840521036da0817ed32
GabrielKyochi <gabriel_k_bernardes@estudante.sesisenai.org.br>
2025-03-21 09:54:47 -0300

    Realização da questão 4, e conserto de detalhes de algumas questões.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Verificação de Par ou Impar:
-let numero = prompt("Digite um número");
+let numero = prompt("Digite um número para verificar se ele é par ou ímpar");
 
 function verificarPar(numero){
     if(numero % 2 === 0){
@@ -13,8 +13,8 @@ verificarPar(numero);
 // Fim da questão 1
 
 // Operações Matemáticas Simples:
-let n1 = parseFloat(prompt("Escreva o n1:"))
-let n2 = parseFloat(prompt("Escreva o n2:"))
+let n1 = parseFloat(prompt("Escreva o n1:"));
+let n2 = parseFloat(prompt("Escreva o n2:"));
 
   function calcular(n1,n2){
 
@@ -40,7 +40,7 @@ let i = 10
 
 function contagem_regressiva(i){
   while (i > 0){
-console.log(i)
+console.log(i);
 i--;   
   }
 }
@@ -48,18 +48,18 @@ contagem_regressiva(i);
 // Fim da questão 3
 
 // Manipulação de Strings:
-var escrita = ["p", "a", "l", "a", "v", "r", "a"];
-
-function inverter_texto(escrita){
-console.log(escrita);
-escrita.reverse();
-console.log(escrita);
+let palavra_inversa = prompt("Escreva uma palavra para ser invertida:")
+function inverterTexto(palavra_inversa){
+    separar_palavras = palavra_inversa.split('');    
+    inverter_palavras = separar_palavras.reverse();
+    juntar_palavras = inverter_palavras.join('');
+    console.log(juntar_palavras);
 }
-inverter_texto(escrita);
+inverterTexto(palavra_inversa);
 // Fim da questão 4
 
 // Contagem de caracteres em um string:
-let palavra = prompt("Escreva uma palavra");
+let palavra = prompt("Escreva uma palavra para contar seus caracteres:");
 let contagem_caracteres = palavra.length
 
 function contar_caracteres(contagem_caracteres){
@@ -75,7 +75,7 @@ let carro = {
   modelo: "Taycan",
   ano: "2025",
   imprimir: function() {
-    console.log("O modelo do carro é:" + this.modelo)
+    console.log("O modelo do carro é:" + this.modelo);
   }
 };
 // Fim da questão 6
@@ -97,7 +97,7 @@ let numero3 = parseFloat(prompt("Digite o terceiro número"));
 
 function media(numero1, numero2, numero3){
   const resultado = (numero1 + numero2 + numero3) / 3;
-  console.log("A média é de:" + resultado)
+  console.log("A média é de:" + resultado);
 }
 media(numero1, numero2, numero3);
 // Fim da questão 8
@@ -113,3 +113,18 @@ for(let i = 1; i <= 20; i++) {
 // Fim da questão 9
 
 // Verificação de Palavra Palíndroma:
+let palavra_ao_contrario = prompt("Escreva uma palavra para verificar se ela é palindroma:");
+palavra_inicial = palavra_ao_contrario;
+function verificar_Palindromo(palavra_ao_contrario){
+    separar_as_palavras = palavra_ao_contrario.split('');    
+    inverter_as_palavras = separar_as_palavras.reverse();
+    juntar_as_palavras = inverter_as_palavras.join('');
+    palavra_final = juntar_as_palavras;
+    if(palavra_inicial = palavra_final){
+       console.log("A palavra é palíndroma!");
+    } else{
+        console.log("A palavra não é palíndroma.");
+    }
+}
+verificar_Palindromo(palavra_ao_contrario);
+// Fim da questão 10
