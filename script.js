1f7c9f0b5cb02265b4a0f01ed5166216867f1529
NarutoKyochi <gabrielkyochi@gmail.com>
2025-03-23 11:57:37 -0300

    Realização do exercício 5 com o length para especificar o número de caracteres da palavra escolhida pelo usuário e um console.log para mostrar em tela essa quantidade.

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 // Exercício 1 - Verificação de Par ou Ímpar:
-    let numero = prompt("Escreva um valor para verificar se ele é par ou ímpar:");
+let numero = prompt("Escreva um valor para verificar se ele é par ou ímpar:");
 
-    function verificar_par(numero){
-        if(numero % 2 === 0){
-            console.log(numero + " é um valor par.");
-        } else{
-            console.log(numero + " é um valor ímpar.");
-        }
+function verificar_par(numero){
+    if(numero % 2 === 0){
+        console.log(numero + " é um valor par.");
+    } else{
+        console.log(numero + " é um valor ímpar.");
+    }
     }
     verificar_par(numero);
 // Encerramento do exercício 1
 
 // Exercício 2 - Operações Matemáticas Simples:
-    let n1 = parseFloat(prompt("Escreva um primeiro número para realizar matemática simples:"));
-    let n2 = parseFloat(prompt("Escreva um segundo número para realizar matemática simples:"));
+let n1 = parseFloat(prompt("Escreva um primeiro número para realizar matemática simples:"));
+let n2 = parseFloat(prompt("Escreva um segundo número para realizar matemática simples:"));
 
-    function calcular(n1, n2){
-        const soma = (n1 + n2);
-        const subtracao = (n1 - n2);
-        const multiplicacao = (n1 * n2);
-        let divisao = 0;
-        if(n2 === 0){
-            console.log("A divisão dos valores é impossível!");
-        } else{
-            divisao = (n1 / n2);
-            console.log("O resultado da divisão dos valores é: " + divisao);
-        }
-        console.log("O resultado da soma dos valores é: " + soma);
-        console.log("O resultado da subtração dos valores é: " + subtracao);
-        console.log("O resultado da multiplicação dos valores é: " + multiplicacao);
+function calcular(n1, n2){
+    const soma = (n1 + n2);
+    const subtracao = (n1 - n2);
+    const multiplicacao = (n1 * n2);
+    let divisao = 0;
+    if(n2 === 0){
+        console.log("A divisão dos valores é impossível!");
+    } else{
+        divisao = (n1 / n2);
+        console.log("O resultado da divisão dos valores é: " + divisao);
+    }
+    console.log("O resultado da soma dos valores é: " + soma);
+    console.log("O resultado da subtração dos valores é: " + subtracao);
+    console.log("O resultado da multiplicação dos valores é: " + multiplicacao);
     }
 calcular(n1, n2);
 // Encerramento do exercício 2
@@ -48,6 +48,7 @@ calcular(n1, n2);
 
 // Exercício 4 - Manipulação de Strings:
 let palavra_inversa = prompt("Escreva uma palavra para ser invertida:");
+
 function inverter_texto(palavra_inversa){
     separar_letras = palavra_inversa.split('');
     inverter_letras = separar_letras.reverse();
@@ -56,4 +57,14 @@ function inverter_texto(palavra_inversa){
 }
 inverter_texto(palavra_inversa);
 // Encerramento do exercício 4
+
+// Exercício 5 -  Contagem de Caracteres em uma String:
+let palavra = prompt("Escreva uma palavra para contar o seu número de caracteres:");
+
+function contar_caracteres(palavra){
+    let contagem_caracteres = palavra.length;
+    console.log("A quantidade de letras presentes na palavra é: " + contagem_caracteres);
+}
+contar_caracteres(palavra);
+// Encerramento do exercício 5
 
