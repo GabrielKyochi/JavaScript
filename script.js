2cb16922a90285600e508e6d01a5ae28b176a7d9
NarutoKyochi <gabrielkyochi@gmail.com>
2025-03-22 23:15:45 -0300

    Desenvolvimento da questão 1, com criação do if para caso  o número digitado pelo usuário seja par e else para caso não seja par.

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Exercício 1 - Verificação de Par ou Ímpar:
     let numero = prompt("Escreva um valor para verificar se ele é par ou ímpar:");
-    function verificar_par(){
-
+    function verificar_par(numero){
+        if(numero %2 === 0){
+            console.log(numero + " é um valor par.");
+        } else{
+            console.log(numero + " é um valor ímpar.");
+        }
     }
+    verificar_par(numero);
