8b2d2185122ef80eb528db5927353d2e29e22a82
NarutoKyochi <gabrielkyochi@gmail.com>
2025-03-23 11:26:36 -0300

    Desenvolvimento do exercício 1, com a atribuição de soma, subtração e multiplicação as suas respectivas constantes e utilização de um if para não aceitar divisões por 0 e linha de código para imprimir no console os resultados.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Exercício 1 - Verificação de Par ou Ímpar:
     let numero = prompt("Escreva um valor para verificar se ele é par ou ímpar:");
+
     function verificar_par(numero){
         if(numero %2 === 0){
             console.log(numero + " é um valor par.");
@@ -11,7 +12,23 @@
 // Encerramento do exercício 1
 
 // Exercício 2 - Operações Matemáticas Simples:
+    let n1 = parseFloat(prompt("Escreva um primeiro número para realizar matemática simples:"));
+    let n2 = parseFloat(prompt("Escreva um segundo número para realizar matemática simples:"));
 
     function calcular(n1, n2){
-        
+        const soma = (n1 + n2);
+        const subtracao = (n1 - n2);
+        const multiplicacao = (n1 * n2);
+        let divisao = 0;
+        if(n2 === 0){
+            console.log("Essa divisão é impossível!");
+        } else{
+            divisao = (n1 / n2);
+        }
+        console.log("O resultado da soma dos valores é: " + soma);
+        console.log("O resultado da subtração dos valores é: " + subtracao);
+        console.log("O resultado da multiplicação dos valores é: " + multiplicacao);
+        console.log("O resultado da divisão dos valores é: " + divisao);
     }
+calcular(n1, n2);
+// Encerramento do exercício 2
